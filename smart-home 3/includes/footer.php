2daaa39fb1dcf2f2e5be0810debe02a5c4e3bfc7
marc-ant0ine <ma.abale10@gmail.com>
2026-04-27 22:29:40 +0200

    Delete smart-home 3 directory

--- smart-home 3/includes/footer.php
+++
@@ -1,9 +0,0 @@
-  </div><!-- /page-content -->
-</main><!-- /main-content -->
-
-</div><!-- /layout -->
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="<?= SITE_URL ?>/js/app.js"></script>
-</body>
-</html>
--- smart-home 3/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'includes/config.php';
-startSession();
-session_destroy();
-header('Location: ' . SITE_URL . '/index.php');
-exit;
