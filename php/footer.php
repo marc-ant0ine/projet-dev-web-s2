216f7e0381f2ebaf3e811cc654a5b8a5694a8da3
marc-ant0ine <ma.abale10@gmail.com>
2026-04-27 22:27:28 +0200

    Delete php directory

--- php/footer.php
+++
@@ -1,7 +0,0 @@
-<?php  ?>
-<footer class="site-footer">
-    <div class="footer-inner">
-        <span class="site-logo-sm">Maison<span>Smart</span></span>
-        <span>Plateforme IoT — Projet ING1 2025-2026</span>
-    </div>
-</footer>
